d98d7b8a28946abac8eba82148b23b800f06358b
disorn-inc <dirsorn1111@hotmail.com>
2021-04-27 20:39:37 +0700

    new version save

--- core_program/gui_realsense_capture/align_cap_numpy_new.py
+++ core_program/gui_realsense_capture/align_cap_numpy_new.py
@@ -9,14 +9,15 @@
 import pyrealsense2 as rs
 # Import Numpy for easy array manipulation
 import numpy as np
+import sys,os
 # Import OpenCV for easy image rendering
 import cv2
 import json
-
+print(os.path.expanduser("~"))
 jsonObj = json.load(open("custom.json"))
 json_string= str(jsonObj).replace("'", '\"')
 #print(json_string)
-path = '/home/disorn/Dataset_test'
+path = os.path.expanduser("~")
 # Create a pipeline
 pipeline = rs.pipeline()
 i = 0
@@ -45,11 +46,11 @@ depth_scale = depth_sensor.get_depth_scale()
 print("Depth Scale is: " , depth_scale)
 
 # We will be removing the background of objects more than
-#  clipping_distance_in_meters mete
-# rs away
-clipping_distance_in_meters = 0.45 #1 meter
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 0.38 #1 meter
 clipping_distance = clipping_distance_in_meters / depth_scale
-
+bg_depth_image = np.zeros((480,640))
+bg_depth_image_raw = np.zeros((480,640))
 # Create an align object
 # rs.align allows us to perform alignment of depth frames to others frames
 # The "align_to" is the stream type to which we plan to align depth frames.
@@ -94,7 +95,7 @@ try:
         depth_filter = hole_filling.process(depth_filter)
         
         
-        colorizer = rs.colorizer()
+        colorizer = rs.colorizer(2)
         depth_image_pre = np.asanyarray(aligned_depth_frame.get_data())
         depth_image = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
         color_image = np.asanyarray(color_frame.get_data())
@@ -106,22 +107,40 @@ try:
         depth_image_3d = np.dstack((depth_image_pre,depth_image_pre,depth_image_pre))
         bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
         depth_bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, depth_image)
-        raw_depth_bg_remove = np.where((depth_image_raw > clipping_distance) | (depth_image_raw <= 0), grey_color, depth_image_raw)
-        depth_image_raw_8 = (depth_image_raw/6).astype('uint8')
+        raw_depth_bg_remove = np.where((depth_image_raw > clipping_distance) | (depth_image_raw <= 0), 0, depth_image_raw)
+        #depth_image_raw_8 = cv2.convertScaleAbs(depth_image_pre, alpha=0.03)
+        depth_image_raw_8 = depth_image_raw.astype('uint8')
+        depth_image_8bit =  depth_image_raw_8 - bg_depth_image
+        depth_image_diff = np.asanyarray(bg_depth_image_raw-depth_image_pre)
+        depth_image_diff_8bit = depth_image_diff.astype('uint8')
+        depth_image_diff_8bit = (np.where((depth_image_pre > bg_depth_image_raw),0,depth_image_diff_8bit)).astype("uint8")
+        depth_image_8bit = (np.where((depth_image_raw_8 < bg_depth_image),0,depth_image_8bit)*10).astype("uint8")
+        im = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_8bit, alpha=0.03),2)
         # Render images
         #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
         #depth_colormap = np.asanyarray(colorizer.colorize(depth_image).get_data())
-        images = np.hstack((color_image, depth_bg_removed))
+        images = np.hstack((color_image,depth_image,bg_removed))
+        images2 = np.hstack((np.dstack((raw_depth_bg_remove.astype('uint8'),raw_depth_bg_remove.astype('uint8'),raw_depth_bg_remove.astype('uint8'))),np.dstack((depth_image_diff_8bit,depth_image_diff_8bit,depth_image_diff_8bit)),depth_bg_removed))
+        images3 = np.vstack((images,images2))
         test = np.dstack((color_image,depth_image_raw_8))
-        print(np.min(depth_image_raw))
+        test1 = np.dstack((color_image,depth_image_diff_8bit))
+        test2 = np.dstack((color_image,raw_depth_bg_remove.astype('uint8')))
+        print(np.min(depth_image_8bit))
         cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('Align Example',images)
+        cv2.imshow('Align Example',cv2.resize(images3,(960,480)))
         key = cv2.waitKey(1)
+        if key == ord("r"):
+            bg_depth_image = depth_image_raw_8
+            bg_depth_image_raw = depth_image_pre
+            np.save(path+'/Dataset/bg_image',bg_depth_image_raw)
         if key == ord("s"):
-            cv2.imwrite(path+'/color_image'+str(i)+'.png', color_image)
-            cv2.imwrite(path+'/depth_image'+str(i)+'.png', depth_image)
-            cv2.imwrite(path+'/fuse_image_8bit'+str(i)+'.png', test)
-            np.save(path+'/depth_image_raw'+str(i), depth_image_raw)
+            cv2.imwrite(path+'/Dataset/color_image'+str(i)+'.png', color_image)
+            cv2.imwrite(path+'/Dataset/bg_remove'+str(i)+'.png',bg_removed)
+            cv2.imwrite(path+'/Dataset/depth_image'+str(i)+'.png', depth_image)
+            cv2.imwrite(path+'/Dataset/fuse_image_8bit'+str(i)+'.png', test)
+            cv2.imwrite(path+'/Dataset/fuse_image_bgremove'+str(i)+'.png', test1)
+            cv2.imwrite(path+'/Dataset/fuse_image_bgremove_v2_'+str(i)+'.png', test2)
+            np.save(path+'/Dataset/depth_image_raw'+str(i), depth_image_raw)
             print(i)
             i=i+1
         # Press esc or 'q' to close the image window
