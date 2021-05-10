6dbe680dd884d7d231546d8397bb6ba0c50f50b7
disorn-inc <dirsorn1111@hotmail.com>
2021-05-10 16:41:53 +0700

    test open 4D image

--- core_program/yolo_part/yolo-camera/open_pic.py
+++ core_program/yolo_part/yolo-camera/open_pic.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2
 import time
 
-camera = cv2.imread('/home/disorn/code_save/Project_Structure/core_program/yolo_part/yolo-camera/test1/depth.png' , cv2.IMREAD_UNCHANGED)
-frame = np.uint8(camera)
-cv2.imshow('j',frame[:,:,0:3])
+camera = cv2.imread('/home/disorn/code_save/Project_Structure/core_program/yolo_part/yolo-camera/test1/fuse_image_bgremove_v2_1.png' , cv2.IMREAD_UNCHANGED)
+frame = (camera)
+cv2.imshow('j',frame)
 print(camera[:,:,0:3])
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
